f4b0af66443bafcec04fe1e867100d5168f1ef89
Bob-Andrews <33861462+Bob-Andrews@users.noreply.github.com>
2024-11-29 21:30:43 +0100

    Ausgabe Userdaten bloß von User im Token nicht von anderen.

--- src/main/java/com/uni/pacmanserver/controller/UserController.java
+++ src/main/java/com/uni/pacmanserver/controller/UserController.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,13 +25,31 @@ public class UserController {
 
     @GetMapping("/{username}")
     public ResponseEntity<User> getUser(@PathVariable String username) {
-        Optional<User> userByName = userRepository.findUserByUsername(username);
 
-        if (userByName.isPresent()) {
-            return new ResponseEntity<User>(userByName.get(), HttpStatus.OK);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return new ResponseEntity("User is not authenticated", HttpStatus.UNAUTHORIZED);
+        } else {
+
+            // Extrahieren des Principal-Objekts
+            Object principal = authentication.getPrincipal();
+            if (principal instanceof UserDetails) {
+                String uName = ((UserDetails) principal).getUsername();
+                if(username.equals(uName)) {
+                    Optional<User> userByName = userRepository.findUserByUsername(username);
+
+                    if (userByName.isPresent()) {
+                        return new ResponseEntity<User>(userByName.get(), HttpStatus.OK);
+                    } else {                
+                        return new ResponseEntity("No User found by username" + username, HttpStatus.NOT_FOUND);
+                    }
+                } else {
+                    return new ResponseEntity("Wrong username for authenticated user", HttpStatus.FORBIDDEN);
+                } 
+            } else {
+                return new ResponseEntity("Invalid authentication principal", HttpStatus.FORBIDDEN);
+            }
         }
-
-        return new ResponseEntity("No User found by username" + username, HttpStatus.NOT_FOUND);
     }
 
     // TODO PatchMapping
